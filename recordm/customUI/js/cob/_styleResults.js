feea415419905b9c5658c07e6cf32916b1eec814
jbarata <joao.barata@cultofbits.com>
2024-01-16 15:25:53 +0000

    fix style regex
    it would give an error in some situations

--- recordm/customUI/js/cob/_styleResults.js
+++ recordm/customUI/js/cob/_styleResults.js
@@ -3,12 +3,12 @@ cob.custom.customize.push(function (core, utils, ui) {
 
     core.customizeColumn("*","*",function(node, esDoc, fieldInfo){
         // Check $styleResultColumn
-        const matchColumnRegex = /\$styleResultColumn\((.*)\)/
+        const matchColumnRegex = /\$styleResultColumn\(([^)]*)\)/
         let styleColField = matchColumnRegex.exec(fieldInfo.fieldDefDescription)
         if(styleColField) {
             const fieldValue = esDoc[toES(fieldInfo.name)] && esDoc[toES(fieldInfo.name)][0];
             const relevantMapping = styleColField[1].split(",")
-    
+
             for(let mapping of relevantMapping) {
                 let [styleValue, styleClass] = mapping.split(":")
                 if( styleValue.trim() == fieldValue
@@ -22,72 +22,72 @@ cob.custom.customize.push(function (core, utils, ui) {
             }
         }
     }),
-    
-    core.customizeAllColumns("*",function(node, esDoc, fieldInfo){
-        // Finds $styleResultRows -> ATTENTION: only first occurrence, subsequent $styleResultRows will be ignore
-        // TODO: remove dependency of $styleResultsRow HAVING TO HAVE ALSO $instanceDescription
-        const matchRowsRegex = /\$styleResultRows\((.*)\)/
-        let firstStyleResultsField = esDoc._definitionInfo.instanceDescription.find( field => matchRowsRegex.exec(field.description))
-        if(firstStyleResultsField) {
-            const fieldValue = esDoc[toES(firstStyleResultsField.name)] && esDoc[toES(firstStyleResultsField.name)][0];
-            const relevantMapping = matchRowsRegex.exec(firstStyleResultsField.description)[1].split(",")
-    
-            for(let mapping of relevantMapping) {
-                let [styleValue, styleClass] = mapping.split(":")
-                if( styleValue.trim() == fieldValue
-                    || styleValue.trim() == "" && fieldValue == undefined
-                    || styleValue.trim() == "*"
-                ) {
-                    node.classList.add(styleClass.trim())
-                    break
+
+        core.customizeAllColumns("*",function(node, esDoc, fieldInfo){
+            // Finds $styleResultRows -> ATTENTION: only first occurrence, subsequent $styleResultRows will be ignore
+            // TODO: remove dependency of $styleResultsRow HAVING TO HAVE ALSO $instanceDescription
+            const matchRowsRegex = /\$styleResultRows\(([^)]*)\)/
+            let firstStyleResultsField = esDoc._definitionInfo.instanceDescription.find( field => matchRowsRegex.exec(field.description))
+            if(firstStyleResultsField) {
+                const fieldValue = esDoc[toES(firstStyleResultsField.name)] && esDoc[toES(firstStyleResultsField.name)][0];
+                const relevantMapping = matchRowsRegex.exec(firstStyleResultsField.description)[1].split(",")
+
+                for(let mapping of relevantMapping) {
+                    let [styleValue, styleClass] = mapping.split(":")
+                    if( styleValue.trim() == fieldValue
+                        || styleValue.trim() == "" && fieldValue == undefined
+                        || styleValue.trim() == "*"
+                    ) {
+                        node.classList.add(styleClass.trim())
+                        break
+                    }
                 }
             }
-        }
-    })
+        })
 
     core.customizeAllInstances((instance, presenter) => {
-      const matcher = /[$]style\[(.*styleField.*)?\]/;  
-      const styleFields = presenter.findFieldPs((fp) => matcher.exec( fp.field.fieldDefinition.description ) );
-      styleFields.forEach((fp) => {
-            
-        const matchColumnRegex = /\$styleResultColumn\((.*)\)/
-        let styleColField = matchColumnRegex.exec(fp.field.fieldDefinition.description)
-        if(styleColField) {
+        const matcher = /[$]style\[(.*styleField.*)?\]/;
+        const styleFields = presenter.findFieldPs((fp) => matcher.exec( fp.field.fieldDefinition.description ) );
+        styleFields.forEach((fp) => {
 
-            const updateStyle = function() {
-              const fieldValue = fp.getValue();
-              const relevantMapping = styleColField[1].split(",")
-              const node = fp.content()[0];
+            const matchColumnRegex = /\$styleResultColumn\(([^)]*)\)/
+            let styleColField = matchColumnRegex.exec(fp.field.fieldDefinition.description)
+            if(styleColField) {
 
-              // Choose node to affect
-              let valueNode = node.querySelector(".radiogroup")
-              if(!valueNode) {
-                valueNode = node.querySelector("td label")
-              } 
+                const updateStyle = function() {
+                    const fieldValue = fp.getValue();
+                    const relevantMapping = styleColField[1].split(",")
+                    const node = fp.content()[0];
 
-              // Clean previous class
-              for(let mapping of relevantMapping) {
-                let [styleValue, styleClass] = mapping.split(":")
-                valueNode.classList.remove(styleClass.trim())
-              }
-    
-              // Set new class
-              for(let mapping of relevantMapping) {
-                  let [styleValue, styleClass] = mapping.split(":")
-                  if( styleValue.trim() == fieldValue
-                      || styleValue.trim() == "" && fieldValue == undefined
-                      || styleValue.trim() == "*"
-                  ) {
-                      valueNode.classList.add(styleClass.trim())
-                      break
-                  }
-              }
+                    // Choose node to affect
+                    let valueNode = node.querySelector(".radiogroup")
+                    if(!valueNode) {
+                        valueNode = node.querySelector("td label")
+                    }
+
+                    // Clean previous class
+                    for(let mapping of relevantMapping) {
+                        let [styleValue, styleClass] = mapping.split(":")
+                        valueNode.classList.remove(styleClass.trim())
+                    }
+
+                    // Set new class
+                    for(let mapping of relevantMapping) {
+                        let [styleValue, styleClass] = mapping.split(":")
+                        if( styleValue.trim() == fieldValue
+                            || styleValue.trim() == "" && fieldValue == undefined
+                            || styleValue.trim() == "*"
+                        ) {
+                            valueNode.classList.add(styleClass.trim())
+                            break
+                        }
+                    }
+                }
+
+                updateStyle();
+                presenter.onFieldChange(fp.field.fieldDefinition.name, updateStyle);
             }
-
-            updateStyle();
-            presenter.onFieldChange(fp.field.fieldDefinition.name, updateStyle);
-        }
-      });
+        });
     });
 
-  })
+})
